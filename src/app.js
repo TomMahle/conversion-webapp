7d71a395e0a725e657726176e1e0e1cadc4d775b
Kathryn Lester <kathrynlester@macbook-pro.attlocal.net>
2021-01-28 16:03:10 -0600

    Start adding elements to app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
 const App = () => {
     return(
-        <div>
-            <h1>HI</h1>
+        <div className="main-container">
+            <div className="internal-container"> 
+                <Form.Control type="number" />
+                <Button>&larr;&rarr;</Button>
+                <Form.Control type="number" />
+            </div>
         </div>
     )
 }
