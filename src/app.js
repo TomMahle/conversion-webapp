dbf2066ec0a8b437b2ff55dc394d456d78bd421e
Kathryn Lester <kathrynlester@macbook-pro.attlocal.net>
2021-01-29 10:39:33 -0600

    Add react router and tabs

--- src/app.js
+++ src/app.js
@@ -1,16 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// https://reactrouter.com/web/guides/quick-start
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    Redirect
+  } from "react-router-dom"; 
 
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
 
 const App = () => {
     return(
         <div className="main-container">
-            <div className="internal-container"> 
-                <Form.Control type="number" />
-                <Button>&larr;&rarr;</Button>
-                <Form.Control type="number" />
+            <div className="mid-container">
+                <Router>
+                    <Nav variant="tabs" defaultActiveKey="/home">
+                        <Nav.Item>
+                            <Link to="/volume">Volume</Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Link to="/weight">Weight</Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Link to="/temperature">Temperature</Link>
+                        </Nav.Item>
+                    </Nav>
+                    <Switch>
+                        <Route exact path="/">
+                            <Redirect to="/volume" />
+                        </Route>
+                        <Route path="/volume">
+                            <div className="internal-container"> 
+                                <Form.Control type="number" min='0' />
+                                <Button>&larr;&rarr;</Button>
+                                <Form.Control type="number" min='0' />
+                            </div>
+                        </Route>
+                        <Route path="/weight">
+                            <div className="internal-container"> 
+                                <Form.Control type="number" min='0' />
+                                <Button>&larr;&rarr;</Button>
+                                <Form.Control type="number" min='0' />
+                            </div>
+                        </Route>
+                        <Route path="/temperature">
+                            <div className="internal-container"> 
+                                <Form.Control type="number" min='0' />
+                                <Button>&larr;&rarr;</Button>
+                                <Form.Control type="number" min='0' />
+                            </div>
+                        </Route>
+                    </Switch>
+                </Router>
             </div>
         </div>
     )
