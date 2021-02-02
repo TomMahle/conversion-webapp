331c1f683c4e8df3ff80ee2374e3d94b7e70adca
Kathryn Lester <kathrynlester@macbook-pro.attlocal.net>
2021-02-01 21:44:51 -0600

    Add useEffect for updating output values

--- src/ConversionForm.jsx
+++ src/ConversionForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
@@ -9,9 +9,16 @@ import { formulas } from './formulas.js';
 const ConversionForm = ({category}) => {
     const [inputVal, setInputVal] = useState(0);
     const [outputVal, setOutputVal] = useState(0);
-    const [{input, output}, setSelectState] = useState({"input": "", "output": ""});
+    const [{input, output}, setSelectState] = useState({"input": Object.keys(formulas[category])[0], "output": Object.keys(formulas[category])[1]})
 
-    // maybe don't need this
+    useEffect(() => {
+        if (category !== "temperature")
+            setOutputVal(inputVal * formulas[category][input][output]);
+        else 
+            setOutputVal(formulas[category][input][output](inputVal));
+        // may need return for "componentWillUnmount"
+    }, [inputVal, outputVal, input, output]);
+
     const swapValues = () => {
         var temp = outputVal;
         setOutputVal(inputVal);
@@ -20,7 +27,7 @@ const ConversionForm = ({category}) => {
 
 
     const options = (io) =>  (
-        <Form.Control as="select" onChange={(e) => setSelectState(currentState => ({...currentState, [io]: e.target.value}))}>
+        <Form.Control as="select" value={io === "input" ? input : output} onChange={(e) => setSelectState(currentState => ({...currentState, [io]: e.target.value}))}>
             {Object.keys(formulas[category]).map((option, index) => {
                 return <option key={index} value={option} >{option}</option>;
             })}
