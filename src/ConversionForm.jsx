dfc93616579e657887e5a4d88c8f4e543db32b04
Kathryn Lester <kathrynlester@macbook-pro.attlocal.net>
2021-02-04 15:02:16 -0600

    Add alert for negative numbers

--- src/ConversionForm.jsx
+++ src/ConversionForm.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
+import Alert from "react-bootstrap/Alert";
 import Form from "react-bootstrap/Form";
 
 import { formulas } from "./formulas.js";
@@ -22,6 +23,13 @@ const ConversionForm = ({ category }) => {
     output: Object.keys(formulas[category])[1],
   });
 
+  // Holds boolean for alert.
+  const [show, setShow] = useState(false);
+
+  // useRef for timeout referenced from ZiiMakc:
+  // https://stackoverflow.com/questions/53090432/react-hooks-right-way-to-clear-timeouts-and-intervals
+  const alertTimeout = useRef(null);
+
   /**
    * Converts a given number from an initial unit to another.
    *
@@ -38,7 +46,23 @@ const ConversionForm = ({ category }) => {
       ...currentState,
       outputVal: getConversion(inputVal, input, output),
     }));
+    return () => {
+      clearTimeout(alertTimeout.current);
+    };
   }, [input, output]);
+
+  /**
+   * Shows alert for two seconds.
+   *
+   * setTimeout functionality referenced from
+   * Andrei Duca: https://stackoverflow.com/questions/56267322/react-hooks-settimeout-after-setstate/56270973
+   */
+  const showAlert = () => {
+    setShow(true);
+    alertTimeout.current = setTimeout(() => {
+      setShow(false);
+    }, 2000);
+  };
 
   /**
    * Returns a react-bootstrap select element with options dependent
@@ -68,40 +92,75 @@ const ConversionForm = ({ category }) => {
     </Form.Control>
   );
 
+  /**
+   * Handles onChange for number input fields.
+   *
+   * @param {String} fromField "inputVal" or "outputVal": set to entered value.
+   * @param {String} toField   "inputVal" or "outputVal": set to calculated value.
+   * @param {Number} fromVal   Specifies number that will be converted.
+   * @param {String} fromUnit  Specifies current unit.
+   * @param {String} toUnit    Specifies unit to convert to.
+   */
+  const handleChange = (fromField, toField, fromVal, fromUnit, toUnit) => {
+    if (!(category === "temperature") && Number(fromVal) < 0) {
+      setVal({
+        [fromField]: fromVal * -1,
+        [toField]: getConversion(fromVal * -1, fromUnit, toUnit),
+      });
+      showAlert();
+    } else {
+      setVal({
+        [fromField]: fromVal,
+        [toField]: getConversion(fromVal, fromUnit, toUnit),
+      });
+    }
+  };
+
   return (
-    <div className="internal-container">
-      <div className="input-dropdown-container">
-        <Form.Control
-          type="number"
-          min="0"
-          value={inputVal}
-          onChange={(e) => {
-            setVal((currentState) => ({
-              ...currentState,
-              inputVal: e.target.value,
-              outputVal: getConversion(e.target.value, input, output),
-            }));
-          }}
-        />
-        {options("input")}
+    <>
+      <Alert id="form-alert" show={show}>
+        Positive numbers only
+      </Alert>
+      <div className="internal-container">
+        <div className="input-dropdown-container">
+          <Form.Control
+            type="number"
+            min="0"
+            value={inputVal}
+            onChange={(e) =>
+              handleChange(
+                "inputVal",
+                "outputVal",
+                e.target.value,
+                input,
+                output
+              )
+            }
+            onClick={(e) => (e.target.value == 0 ? e.target.select() : null)}
+          />
+          {options("input")}
+        </div>
+        <div className="equal-sign">=</div>
+        <div className="input-dropdown-container">
+          <Form.Control
+            type="number"
+            min="0"
+            value={outputVal}
+            onChange={(e) =>
+              handleChange(
+                "outputVal",
+                "inputVal",
+                e.target.value,
+                output,
+                input
+              )
+            }
+            onClick={(e) => (e.target.value == 0 ? e.target.select() : null)}
+          />
+          {options("output")}
+        </div>
       </div>
-      <div className="equal-sign">=</div>
-      <div className="input-dropdown-container">
-        <Form.Control
-          type="number"
-          min="0"
-          value={outputVal}
-          onChange={(e) => {
-            setVal((currentState) => ({
-              ...currentState,
-              outputVal: e.target.value,
-              inputVal: getConversion(e.target.value, output, input),
-            }));
-          }}
-        />
-        {options("output")}
-      </div>
-    </div>
+    </>
   );
 };
 
